08848be53a1abdfd8599908a490adcb3efce4108
Mell <prizrak0055@mail.ru>
2023-02-28 12:47:39 +0300

    for merge request

--- Gossip/gossip.cc
+++ Gossip/gossip.cc
@@ -1,37 +1,42 @@
 #pragma once
 #include "gossip.hh"
-
+//
 Gossip::Gossip(sstring local_adderss) {
   peer_local_.SetPeerIpAddr(local_adderss);
 }
-
+//
 Peer Gossip::AddPeer(int peer_id, sstring ip_addr) {
   peer_local_.SetPeerId(peer_id);
   peer_local_.SetPeerIpAddr(ip_addr);
 }
+//
 void Gossip::SetLocalPayload(Payload payload) {
   peer_local_.SetPeerPayload(payload);
 }
+//
 void Gossip::DelPeer(int host_id) {
   for (auto it = peers_.payload.begin(); it != peers_.payload.end(); ++it) {
     if ((*it) == host_id) {
-      peers_.payload.erase(it);
+      peers_.payload.erase(it); //
       return;
     }
   }
 }
-
+//
 void Peer::SetPeerPayload(Payload add_payload) {
   payload_.Epoch = add_payload.Epoch;
   payload_.Blob = std::move(add_payload.Blob);
 }
+//
 Payload Peer::GetPeerPayload() { return payload_; }
-
+//
 int Peer::GetPeerId() { return peer_id_; }
-
+//
 sstring Peer::GetPeerIpAddr() { return peer_ip_addr_; }
 void Peer::SetPeerId(int peer_id) { peer_id_ = peer_id; }
-void Peer::SetPeerIpAddr(sstring peer_ip_addr) { peer_ip_addr_ = peer_ip_addr; }
+void Peer::SetPeerIpAddr(sstring peer_ip_addr) {
+  peer_ip_addr_ = peer_ip_addr;
+} //
 void Peer::SetPeerClient(
     std::unique_ptr<rpc::protocol<serializer>::client> client) {
   client_ = std::make_unique<rpc::protocol<serializer>::client>(client);
--- Gossip/gossip.hh
+++ Gossip/gossip.hh
@@ -4,44 +4,46 @@
 #include <seastar/core/loop.hh>
 #include <seastar/core/reactor.hh>
 #include <seastar/rpc/rpc.hh>
-
+//
 using namespace seastar;
-
+//
 struct serializer {};
-
+//
 struct Payload {
-  uint64_t Epoch; // number of epoch
+  uint64_t Epoch; // number of epoch //
   std::vector<char> Blob;
 };
+//
 struct Config {
   std::vector<int> payload;
 };
-
+//
 class Gossip {
 public:
   Gossip(sstring local_address);
-  Peer AddPeer(int peer_id, sstring ip_addr);
+  Peer AddPeer(int peer_id, sstring ip_addr); //
   void SetLocalPayload(Payload payload);
   void DelPeer(int host_id);
-
+  //
 private:
   Peer peer_local_;
   Config peers_;
 };
-
+//
 class Peer {
 public:
   void SetPeerPayload(Payload add_payload);
   Payload GetPeerPayload();
   int GetPeerId();
-  sstring GetPeerIpAddr();
+  sstring GetPeerIpAddr(); //
   void SetPeerId(int peer_id);
   void SetPeerIpAddr(sstring ip_addr);
   void SetPeerClient(std::unique_ptr<rpc::protocol<serializer>::client> client);
-
+  //
 private:
   Payload payload_;
   int peer_id_;
   sstring peer_ip_addr_;
   std::unique_ptr<rpc::protocol<serializer>::client> client_;
 };
+//
