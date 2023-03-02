65b6b4fa0257ac5d328fd6c2a96adafaec717d06
Mell <prizrak0055@mail.ru>
2023-03-02 13:02:39 +0300

    Fix type peer_id

--- Gossip/gossip.hh
+++ Gossip/gossip.hh
@@ -4,46 +4,45 @@
 #include <seastar/core/loop.hh>
 #include <seastar/core/reactor.hh>
 #include <seastar/rpc/rpc.hh>
-//
+
 using namespace seastar;
-//
+using PeerId = uint64_t;
 struct serializer {};
-//
+
 struct Payload {
-  uint64_t Epoch; // number of epoch //
-  std::vector<char> Blob;
+  uint64_t epoch; // number of epoch
+  std::vector<char> blob;
 };
-//
+
 struct Config {
-  std::vector<int> payload;
+  std::vector<PeerId> payload;
 };
-//
+
 class Gossip {
 public:
   Gossip(sstring local_address);
-  Peer AddPeer(int peer_id, sstring ip_addr); //
+  Peer AddPeer(int peer_id, sstring ip_addr);
   void SetLocalPayload(Payload payload);
-  void DelPeer(int host_id);
-  //
+  void DelPeer(PeerId peer_id);
+
 private:
   Peer peer_local_;
   Config peers_;
 };
-//
+
 class Peer {
 public:
   void SetPeerPayload(Payload add_payload);
   Payload GetPeerPayload();
   int GetPeerId();
-  sstring GetPeerIpAddr(); //
+  sstring GetPeerIpAddr();
   void SetPeerId(int peer_id);
   void SetPeerIpAddr(sstring ip_addr);
   void SetPeerClient(std::unique_ptr<rpc::protocol<serializer>::client> client);
-  //
+
 private:
   Payload payload_;
-  int peer_id_;
+  PeerId peer_id_;
   sstring peer_ip_addr_;
   std::unique_ptr<rpc::protocol<serializer>::client> client_;
 };
-//
